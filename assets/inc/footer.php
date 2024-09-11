ad027141f75fcf74867a444c3ad9ec09dc8fed14
Davalorlk <107067508+Davalorlk@users.noreply.github.com>
2024-09-11 20:40:01 +0100

    Add files via upload

---
+++ assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                2024 - <?php echo date('Y'); ?> &copy; Hospital Management Information System. Developed By ICT UNIVERSITY STUDENTS LEVEL 2 GROUP 8</a>
+            </div>
+
+        </div>
+    </div>
+</footer>
