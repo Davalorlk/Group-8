a7ffa584238205e9913ed22c56aca70d8550ca4d
Davalorlk <107067508+Davalorlk@users.noreply.github.com>
2024-09-11 19:29:54 +0100

    Delete backend directory

--- backend/his_doc_logout_partial.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['doc_id']);
-    unset($_SESSION['doc_number']);
-    session_destroy();
-
-    header("Location: his_doc_logout.php");
-    exit;
-?>
